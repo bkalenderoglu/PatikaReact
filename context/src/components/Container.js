b90740b3bfc575383c3d0d33ee1132740dc55d20
bkalenderoglu <bkalenderoglu@st.medipol.edu.tr>
2021-09-29 17:30:01 +0300

    context is completed

---
+++ context/src/components/Container.js
@@ -0,0 +1,22 @@
+import Button from './Button'
+import Header from './Header'
+
+import {useTheme} from '../context/ThemeContext'
+import Profile from './Profile';
+
+function Container() {
+    const { theme } = useTheme();
+    console.log(theme);
+    return (
+    //  <div className={`app ${theme}`}>
+        <div className={`app ${theme === 'dark' ? theme : ""}`}>
+            <Header />
+            <hr />
+            <Button />
+            <hr />
+            <Profile />
+        </div>
+    )
+}
+
+export default Container
