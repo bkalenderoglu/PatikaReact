23356d5576a10a8225418976632351c77b98b506
bkalenderoglu <bkalenderoglu@st.medipol.edu.tr>
2021-10-05 14:30:39 +0300

    localization completed

---
+++ localization-intl/src/App.js
@@ -0,0 +1,42 @@
+import './App.css';
+import { IntlProvider, FormattedMessage } from "react-intl"
+import { useState, useEffect } from 'react';
+
+const messages = {
+  "tr-TR": {
+    title: "Merhaba Dünya",
+    description: "{count} yeni mesajınız var"
+  },
+  "en": {
+    title: "Hello World",
+    description: " You have {count} new messages"
+  },
+
+};
+
+function App() {
+
+  const isLocale = localStorage.getItem("locale");
+  const defaultLocale = isLocale ? isLocale : navigator.language;
+  const [locale, setLocale] = useState(defaultLocale);
+
+  useEffect(() => {
+    localStorage.setItem("locale", locale);
+  }, [locale]);
+
+  return (
+    <div className="App">
+      <IntlProvider locale={locale} messages={messages[locale]}>
+          <FormattedMessage id="title"  />
+          <p>
+            <FormattedMessage id="description" values={{count: 5}} />
+          </p>
+          <br /><br />
+          <button onClick={() => setLocale("tr-TR")}>TR</button>
+          <button onClick={() => setLocale("en")}>EN</button>
+      </IntlProvider>
+    </div>
+  );
+}
+
+export default App;
